e4035607aae368642c979baa954746105b7b93a0
Trey Vincent <treyvincent@hotmail.com>
2023-09-04 12:29:43 -0500

    Update imports for profile page

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,5 +1,5 @@
-import PostThread from '@/components/forms/PostThread';
-import { fetchUser } from '@/lib/actions/user.actions';
+
+import { getUser } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
@@ -9,7 +9,7 @@ async function Page() {
     
     if(!user) return null;
     
-    const userInfo = await fetchUser(user.id);
+    const userInfo = await getUser(user.id);
 
     if(!userInfo?.onboarded) redirect('/onboarding');
 
