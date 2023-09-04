5465f51f744fcf5cf15af638e5c0630e98155b23
Trey Vincent <treyvincent@hotmail.com>
2023-09-04 12:50:41 -0500

    Commenting out code for vercel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,16 @@
 
 import { getUser } from "@/lib/actions/user.actions";
-import { UserButton } from "@clerk/nextjs";
+
 import { currentUser } from '@clerk/nextjs'
 import { redirect } from "next/navigation";
 
 export default async function Home() {
 
-  const user = await currentUser();
-  if (!user) return null;
+  // const user = await currentUser();
+  // if (!user) return null;
 
-  const userInfo = await getUser(user.id);
-  if (!userInfo?.onboarded) redirect("/onboarding");
+  // const userInfo = await getUser(user.id);
+  // if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
     <>
