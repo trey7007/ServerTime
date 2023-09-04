612b16ab82b6474686ed43057f081e5facc11526
Trey Vincent <treyvincent@hotmail.com>
2023-09-04 13:35:59 -0500

    Determining what the issue with Vercel is

--- app/(root)/staffing/page.tsx
+++ app/(root)/staffing/page.tsx
@@ -1,3 +1,4 @@
+import WorkerCard from '@/components/cards/WorkerCard';
 import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
@@ -11,7 +12,12 @@ async function Page() {
         <>
         
         <h1 className= "head-text">Staffing</h1>
-        
+        <WorkerCard
+            key={"64f4ca7eda1ed5f9cd185c35"}
+            id = {"64f4ca7eda1ed5f9cd185c35"}
+            firstname = {"Trey"}
+            lastname = {"Vincent"}
+          />
 
         </>
 
